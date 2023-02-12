ff0204673a03d631a90195b132a3b15bd8a80c6d
yusufstawan <yusufsetiyawan45@gmail.com>
2023-02-12 20:03:03 +0700

    update port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 9000,
+    port: 5000,
     host: 'localhost',
     routes: {
       cors: {
